a0d5aac62a68a8456bed6f5204b0edfd60c8d5d2
Mehedi Hassan <mehedi.dip@outlook.com>
2016-12-17 12:31:46 +0000

    Navigation base

--- functions.php
+++ functions.php
@@ -102,14 +102,97 @@ add_action( 'widgets_init', 'corona_widgets_init' );
  * Enqueue scripts and styles.
  */
 function corona_scripts() {
-	wp_enqueue_style( 'corona-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'corona-style', get_stylesheet_uri(), true, '1.0', 'all');
 
 	wp_enqueue_style( 'corona-base-css', get_template_directory_uri() . '/inc/css/winstrap.css', true, '1.0', 'all' );
 
-	wp_enqueue_script( 'corona-main', get_template_directory_uri() . '/inc/js/app.js', array(), '20151215', true );
+	wp_enqueue_script( 'corona-main', get_template_directory_uri() . '/inc/js/app.js', array( 'jquery' ), true );
 
-	wp_enqueue_script( 'corona-base-js', get_template_directory_uri() . '/inc/js/winstrap.js', array(), '20151215', true );
+	wp_enqueue_script( 'corona-base-js', get_template_directory_uri() . '/inc/js/winstrap.js', array( 'jquery' ), true );
+	
 }
 
 add_action( 'wp_enqueue_scripts', 'corona_scripts' );
 
+
+function corona_excerpt($limit) {
+    $excerpt = get_the_excerpt($post->ID);
+    if(strlen($excerpt) > $limit) {
+        $excerpt = substr($excerpt, 0, $limit) . '...';
+    }
+
+    echo $excerpt;
+}
+
+//navigation
+function corona_get_nav( $theme_location ) {
+    if ( ($theme_location) && ($locations = get_nav_menu_locations()) && isset($locations[$theme_location]) ) {
+         
+        $menu_list  = '<nav class="navbar navbar-default">' ."\n";
+		$menu_list .= '<div class="navbar-local color-accent theme-dark">' ."\n";
+        $menu_list .= '<div class="container">' ."\n";
+        $menu_list .= '<div class="navbar-header">' ."\n";
+        $menu_list .= '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">' ."\n";
+        $menu_list .= '<span class="sr-only">Toggle navigation</span>' ."\n";
+        $menu_list .= '<span class="icon-bar"></span>' ."\n";
+        $menu_list .= '<span class="icon-bar"></span>' ."\n";
+        $menu_list .= '<span class="icon-bar"></span>' ."\n";
+        $menu_list .= '</button>' ."\n";
+        $menu_list .= '<a class="navbar-brand" href="' . home_url() . '">' . get_bloginfo( 'name' ) . '</a>';
+        $menu_list .= '</div>' ."\n";
+                 
+         
+        $menu = get_term( $locations[$theme_location], 'nav_menu' );
+        $menu_items = wp_get_nav_menu_items($menu->term_id);
+ 
+        $menu_list .= '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">' ."\n";
+        $menu_list .= '<ul class="nav navbar-nav navbar-right">' ."\n";
+          
+        foreach( $menu_items as $menu_item ) {
+            if( $menu_item->menu_item_parent == 0 ) {
+                 
+                $parent = $menu_item->ID;
+                 
+                $menu_array = array();
+                foreach( $menu_items as $submenu ) {
+                    if( $submenu->menu_item_parent == $parent ) {
+                        $bool = true;
+                        $menu_array[] = '<li><a href="' . $submenu->url . '">' . $submenu->title . '</a></li>' ."\n";
+                    }
+                }
+                if( $bool == true && count( $menu_array ) > 0 ) {
+                     
+                    $menu_list .= '<li class="dropdown">' ."\n";
+                    $menu_list .= '<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' . $menu_item->title . ' <span class="caret"></span></a>' ."\n";
+                     
+                    $menu_list .= '<ul class="dropdown-menu">' ."\n";
+                    $menu_list .= implode( "\n", $menu_array );
+                    $menu_list .= '</ul>' ."\n";
+                     
+                } else {
+                     
+                    $menu_list .= '<li>' ."\n";
+                    $menu_list .= '<a href="' . $menu_item->url . '">' . $menu_item->title . '</a>' ."\n";
+                }
+                 
+            }
+             
+            // end <li>
+            $menu_list .= '</li>' ."\n";
+        }
+          
+        $menu_list .= '</ul>' ."\n";
+        $menu_list .= '</div>' ."\n";
+        $menu_list .= '</div><!-- /.container-fluid -->' ."\n";
+        $menu_list .= '</nav>' ."\n";
+  
+    } else {
+        $menu_list = '<!-- no menu defined in location "'.$theme_location.'" -->';
+    }
+     
+    echo $menu_list;
+}
+
+
+//disable admin bar
+show_admin_bar(false);
--- page.php
+++ page.php
@@ -20,13 +20,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'inc/content/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
+				
 			endwhile; // End of the loop.
 			?>
 
