0ac8e9073d1334599ce41477e637343cab126c9b
Mehedi Hassan <mehedi.dip@outlook.com>
2016-12-18 10:26:50 +0000

    Categories, Authors, Tags

--- archive.php
+++ archive.php
@@ -11,41 +11,65 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<!--Main Block-->
+			<div class="corona-main latest-block">
+                <div class="container">
+                    <div class="corona-latest col-md-16">
+                        
+                        <div class="corona-article-list">
+                            <?php
+								if ( have_posts() ) : ?>
+									<?php the_archive_title( '<div class="title">', '</div>' ); ?>
 
-		<?php
-		if ( have_posts() ) : ?>
+									<?php while ( have_posts() ) : the_post(); ?>
+ 									
+									 <article>  
+                                            <?php 
+                                                $perma = get_permalink();
+                                                $title = get_the_title();
+                                                $author = get_the_author();
+                                                $authorURL = get_author_posts_url( get_the_author_meta( 'ID' ));
+                                                $thumb = get_the_post_thumbnail_url($medium);
+                                                if(!$thumb){
+                                                    $thumb = get_template_directory_uri() . '/inc/assets/missing.png';
+                                                }
+                                            ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+                                            <div class="inner">
+                                                <a title="<?php echo $title; ?>" href="<?php echo $perma; ?>">
+                                                    <div class="thumb" style="background-image: url('<?php echo $thumb; ?>');">
+                                                    </div>
+                                                </a>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+                                                <div class="meta">
+                                                    <div class="sub">
+                                                        <a title="<?php echo $title; ?>" href="<?php echo $perma; ?>"><h2><?php echo $title; ?></h2></a>
+                                                        <p class="info"><a title="<?php echo 'Read other articles by ' . $author; ?>" href="<?php echo $authorURL ?>">by <?php echo $author; ?></a>, <?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ); ?> ago</p>
+                                                    </div>
+                                                    <p><?php echo corona_excerpt(200); ?></p>
+                                                </div>
+                                            </div>
+                                        </article>
+								<?php endwhile; ?>
+										<nav class="corona-loop-nav">
+                                            <div class="btn-left"><?php echo get_next_posts_link( 'Older Articles', $latest->max_num_pages ); // display older posts link ?></div>
+                                            <div class="btn-right"><?php echo get_previous_posts_link( 'Newer Articles'); // display newer posts link ?></div>
+                                        </nav>
+								<?php else :
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+									get_template_part( 'template-parts/content', 'none' );
 
-			endwhile;
+								endif; 
+							?>
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
+                        </div>
+                    </div>
+                    <div class="corona-sidebar col-md-8">
+                        <?php echo get_sidebar(); ?>
+                    </div>
+                </div>
+            </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php
-get_sidebar();
 get_footer();
